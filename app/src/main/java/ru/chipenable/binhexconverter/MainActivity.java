ef4fc67c1db1dab02c30ea784883abb32b5cce7b
Pavel Bobkov <microsmaster@rambler.ru>
2016-05-23 16:19:02 +0300

    links view and presenter. the app converts numbers now

--- app/src/main/java/ru/chipenable/binhexconverter/MainActivity.java
+++ app/src/main/java/ru/chipenable/binhexconverter/MainActivity.java
@@ -4,13 +4,17 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.Spannable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements IConverterView {
+public class MainActivity extends AppCompatActivity implements IConverterView,
+        View.OnFocusChangeListener {
 
     @BindView(R.id.dec_edit_text)EditText mDecEditText;
     @BindView(R.id.hex_edit_text)EditText mHexEditText;
@@ -18,15 +22,58 @@ public class MainActivity extends AppCompatActivity implements IConverterView {
 
     private IConverterPresenter mPresenter;
 
+    private TextWatcher mDecTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            mPresenter.convertDec(s.toString());
+        }
+    };
+
+    private TextWatcher mHexTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            mPresenter.convertHex(s.toString());
+        }
+    };
+
+    private TextWatcher mBinTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            mPresenter.convertBin(s.toString());
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        Repository repository = new Repository(this.getPreferences(Activity.MODE_PRIVATE));
-        Model model = new Model(repository);
+        IConverterRepository repository = new Repository(this.getPreferences(Activity.MODE_PRIVATE));
+        IConverterModel model = new Model(repository);
         mPresenter = new Presenter(this, model);
+
+        mDecEditText.setOnFocusChangeListener(this);
+        mHexEditText.setOnFocusChangeListener(this);
+        mBinEditText.setOnFocusChangeListener(this);
     }
 
     @Override
@@ -75,5 +122,26 @@ public class MainActivity extends AppCompatActivity implements IConverterView {
         String id = Integer.toString(view.getId());
         String data = mPresenter.restoreData(id);
         view.setText(data);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        mDecEditText.removeTextChangedListener(mDecTextWatcher);
+        mHexEditText.removeTextChangedListener(mHexTextWatcher);
+        mBinEditText.removeTextChangedListener(mBinTextWatcher);
+
+        setTextWatcher(mDecEditText, mDecTextWatcher);
+        setTextWatcher(mHexEditText, mHexTextWatcher);
+        setTextWatcher(mBinEditText, mBinTextWatcher);
+    }
+
+
+
+    /********************** util functions ***************************/
+
+    private void setTextWatcher(EditText editText, TextWatcher textWatcher){
+        if (editText.isFocused()){
+            editText.addTextChangedListener(textWatcher);
+        }
     }
 }
